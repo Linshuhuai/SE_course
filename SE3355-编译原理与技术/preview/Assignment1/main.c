bf5af006707458a02507a8484318706c6d69e6fd
Linshuhuai <68037461+Linshuhuai@users.noreply.github.com>
2021-09-07 20:07:08 +0800

    Add files via upload

---
+++ SE3355-编译原理与技术/preview/Assignment1/main.c
@@ -0,0 +1,12 @@
+/* This file is intentionally empty.  You should fill it in with your
+   solution to the programming exercise. */
+#include "prog1.h"
+#include <stdio.h>
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
+
+
---
+++ SE3355-编译原理与技术/preview/Assignment1/prog1.h
@@ -0,0 +1,4 @@
+/*本无头文件，但编译有问题，遂作修改*/
+#include "slp.h"
+
+A_stm prog(void);
